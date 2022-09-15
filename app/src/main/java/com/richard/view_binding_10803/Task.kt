2ba7f2e772e1f008adaa2c42f9f8009083338d2a
RichardsBR <raffaellorichard52@gmail.com>
2022-09-15 08:37:27 +0700

    GD4_10803

---
+++ app/src/main/java/com/richard/view_binding_10803/Task.kt
@@ -0,0 +1,3 @@
+package com.richard.view_binding_10803
+
+data class Task (val chapter : String, val title : String, val details : String)
---
+++ app/src/main/java/com/richard/view_binding_10803/TaskList.kt
@@ -0,0 +1,11 @@
+package com.richard.view_binding_10803
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Tutorial Main Genshin","Cara bermain genshin dengan benar"),
+        Task("Chapter 2","Buka Genshin","Menyelesaikan daily kemudian nguli primogems"),
+        Task("Chapter 3","Ngga tau harus ngapain","Menggabut karena kehabisa konten"),
+        Task("Chapter 4","Ngepull","Menghabiskan semua primogems tapi kalah 50%/50%"),
+        Task("Chapter 5","Penutup","Menangis di pojokan karena ngga dapet Staff of Homeless")
+    )
+}
